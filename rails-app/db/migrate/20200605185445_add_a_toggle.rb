568675e817fe4cba8f7bdd2eb4594e2e51b3bdf3
Justin Spencer <justin.spencer@icf.com>
2020-06-12 17:03:29 -0400

    notes on tooling

--- rails-app/db/migrate/20200605185445_add_a_toggle.rb
+++ rails-app/db/migrate/20200605185445_add_a_toggle.rb
@@ -1,5 +1,9 @@
 class AddAToggle < ActiveRecord::Migration[6.0]
-  def change
-    
+  def up
+    FeatureAdapter.insert_new_feature(name: "ft_something_2", description: "something")
+  end
+
+  def down
+    FeatureAdapter.delete_feature(name: "ft_something_2")
   end
 end
